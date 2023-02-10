cfcc1e537d77016eb826260a625b636568734c84
vaibhav chand rana <87354720+vaibhavchandrana@users.noreply.github.com>
2023-02-10 09:29:11 +0000

    add changes

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import '../css/home.css'
-import img from '../images/voting_image'
+// import img from '../images/voting_image2.jpg'
 
 export const Home = () => {
-  return (
-    <div className='home_container'>
-        <div className='content_container'></div>
-        <div className='image-div'>
-            <img src={img} alt="voting img" />
+    return (
+        <div className='body'>
+            <div className='home_container'>
+                <div className='content_container'>
+                    <span>Take Charge of
+                        <br/>
+                        Your Vote</span>
+                </div>
+                <div className='image-div'>
+                    <img src={"https://www.gifimages.pics/images/quotes/english/general/gif-for-voting-52650-149181.gif"} alt="voting img" />
+                </div>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
