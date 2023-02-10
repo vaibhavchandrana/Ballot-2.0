78b8691ffe4cc8937916daf280e5f7e37e2d2bc6
vaibhav chand rana <87354720+vaibhavchandrana@users.noreply.github.com>
2023-02-10 08:58:54 +0000

    add

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../css/home.css'
+
+export const Home = () => {
+  return (
+    <div className='home_container'>
+        <div className='content_container'></div>
+        <div className='image-div'></div>
+    </div>
+  )
+}
+
