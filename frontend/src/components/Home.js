de1921fd850222dba1610b1dc7b86e847154ad03
vaibhav chand rana <87354720+vaibhavchandrana@users.noreply.github.com>
2023-02-10 09:07:16 +0000

    add

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import '../css/home.css'
+import img from '../images/voting_image'
 
 export const Home = () => {
   return (
     <div className='home_container'>
         <div className='content_container'></div>
-        <div className='image-div'></div>
+        <div className='image-div'>
+            <img src={img} alt="voting img" />
+        </div>
     </div>
   )
 }
